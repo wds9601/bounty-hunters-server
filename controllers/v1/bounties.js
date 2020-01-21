2906a640baabe90adb92e550b21c349d0067cdf6
William Downey-Schell <Will@Williams-MacBook-Air.local>
2020-01-21 12:04:19 -0800

    created basic app routes

--- controllers/v1/bounties.js
+++ controllers/v1/bounties.js
@@ -1,9 +1,39 @@
 //Create router and reference the models
 let router = require('express').Router()
 
-//GET /bounties - retrieve all bounties in DB
+//GET v1/bounties - retrieve all bounties in DB
 router.get('/', (req, res) => {
     res.send('TBD - get the bounties')
+})
+
+//POST /v1/bounties - Create a new bounty
+router.post('/', (req, res) => {
+    res.send('TBD - create a bounty')
+})
+
+//PUT /v1/bounties - Bulk Update ALL bounties
+router.put('/', (req, res) => {
+    res.send('TBD - Update all the bounties')
+})
+
+//DELETE /v1/bounties - Delete ALL bounties
+router.delete('/', (req, res) => {
+    res.send('TBD - Delete all the bounties')
+})
+
+//GET v1/bounties/:id - Retrieve a single bounty by its ID
+router.get('/:id', (req, res) => {
+    res.send('TBD - Get one bounty')
+})
+
+//PUT /v1/bounties/:id - Update a single bounty
+router.put('/:id', (req, res) => {
+    res.send('TBD - Update one bounty')
+})
+
+//DELETE /v1/bounties/:id - Delete a single bounty
+router.delete('/:id', (req, res) => {
+    res.send('TBD - Delete one bounty')
 })
 
 //Export the router object and the routes attached to it
