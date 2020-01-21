2a42e7423ef5bc6d853a17bc301b06db99e23e74
William Downey-Schell <Will@Williams-MacBook-Air.local>
2020-01-21 14:24:31 -0800

    Routes excpet for PUT all implemented

--- controllers/v1/bounties.js
+++ controllers/v1/bounties.js
@@ -16,7 +16,18 @@ router.get('/', (req, res) => {
 
 //POST /v1/bounties - Create a new bounty
 router.post('/', (req, res) => {
-    res.send('TBD - create a bounty')
+    db.Bounty.create(req.body)
+    .then(newBounty => {
+        res.status(201).send(newBounty)
+    })
+    .catch(err => {
+        console.log('ERROR in POST /bounties', err)
+        if (err.name === 'ValidationError') {
+            res.status(406).send({message: 'Validation Error'})
+        } else {
+            res.status(503).send({message: 'Database or Server Error'})
+        }
+    })
 })
 
 //PUT /v1/bounties - Bulk Update ALL bounties
@@ -26,7 +37,14 @@ router.put('/', (req, res) => {
 
 //DELETE /v1/bounties - Delete ALL bounties
 router.delete('/', (req, res) => {
-    res.send('TBD - Delete all the bounties')
+    db.Bounty.deleteMany()
+    .then(() => {
+        res.status(204).send()
+    })
+    .catch(err => {
+        console.log('Error in delete all route', err)
+        res.status(503).send({message: 'Server-side error'})
+    })
 })
 
 //GET v1/bounties/:id - Retrieve a single bounty by its ID
@@ -47,12 +65,32 @@ router.get('/:id', (req, res) => {
 
 //PUT /v1/bounties/:id - Update a single bounty
 router.put('/:id', (req, res) => {
-    res.send('TBD - Update one bounty')
+    db.Bounty.findOneAndUpdate({
+        _id: req.params.id
+    },
+    req.body,
+    {
+        new: true //establishes that this updated item is the 'updated' result used in 'updatedBounty'
+    })
+    .then(updatedBounty => {
+        res.send(updatedBounty)
+    })
+    .catch(err => {
+        console.log('Error in PUT /bounty/:id', err)
+        res.status(503).send({message: 'Server-side error'})
+    })
 })
 
 //DELETE /v1/bounties/:id - Delete a single bounty
 router.delete('/:id', (req, res) => {
-    res.send('TBD - Delete one bounty')
+    db.Bounty.findByIdAndDelete(req.params.id)
+    .then(() => {
+        res.send(204).send() //no message bc error is 'no content'
+    })
+    .catch(err => {
+        res.status(503).send({message: 'Server-side error', err})
+        console.log('Error when deleting ONE bounty', err)
+    })
 })
 
 //Export the router object and the routes attached to it
